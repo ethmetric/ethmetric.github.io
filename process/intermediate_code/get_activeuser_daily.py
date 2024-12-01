6ab5e6a01be0ed57ec291466ba4e8933133706e5
tczpl <tczpl@163.com>
2024-12-01 20:06:43 +0800

    active user

--- process/intermediate_code/get_activeuser_daily.py
+++ process/intermediate_code/get_activeuser_daily.py
@@ -1,9 +1,10 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from util import datadir,sort_by_blocknum,to_int,to_str
+from util import datadir, sort_by_blocknum, date_to_day_time, date_to_last_block, prune_files, read_first_line_gt_block
 import glob
 import functools
+import time
 
 output_dir = "intermediate_data/activeuser_daily/"
 try:
@@ -14,19 +15,35 @@ except:
 BlockTransactionCsvs = glob.glob(datadir+"*BlockTransaction.csv")
 BlockTransactionCsvs.sort(key=functools.cmp_to_key(sort_by_blocknum))
 
-print(datadir, BlockTransactionCsvs)
 
 last_write_daytime = 0
+
+only_update = False
+files = os.listdir(output_dir)
+files.sort()
+only_update = len(files) > 0
+
+if only_update:
+    last_date = files[-1].split(".")[0]
+    last_write_daytime = date_to_day_time(last_date) + 86400
+    last_block = date_to_last_block(last_date)
+    print("only update from", last_block)
+    BlockTransactionCsvs = prune_files(BlockTransactionCsvs, last_block)
+
+print(datadir, BlockTransactionCsvs)
+
 activeusers = set()
 
-import time
-start = time.time()
 
 for file in BlockTransactionCsvs:
     theCSV = open(file)
     head = theCSV.readline().strip()
 
-    oneLine = theCSV.readline().strip()
+    if only_update:
+        oneLine = read_first_line_gt_block(theCSV, last_block)
+    else:
+        oneLine = theCSV.readline().strip()
+    
     while oneLine!="":
         oneArray = oneLine.split(",")
 
