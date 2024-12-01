427beb1e85e84c6f91d74e3f788742d5a668affb
tczpl <tczpl@163.com>
2024-12-02 01:06:48 +0800

    usdtupdate

--- process/intermediate_code/get_usdtupdate_daily.py
+++ process/intermediate_code/get_usdtupdate_daily.py
@@ -1,9 +1,10 @@
 import sys
 import os
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-from util import datadir,sort_by_blocknum,to_int,to_str, dao_hardfork_beneficiary, dao_hardfork_accounts
+from util import datadir, sort_by_blocknum, date_to_day_time, date_to_last_block, prune_files, read_first_line_gt_block
 import glob
 import functools
+import time
 
 output_dir = "intermediate_data/usdtupdate_daily/"
 try:
@@ -15,14 +16,35 @@ ERC20TransactionCsvs = glob.glob(datadir+"*ERC20Transaction.csv")
 ERC20TransactionCsvs.sort(key=functools.cmp_to_key(sort_by_blocknum))
 
 
-print(datadir, ERC20TransactionCsvs)
 
 last_write_daytime = 0
 usdt_of = {}
 usdtupdate_of = {}
 
-import time
-start = time.time()
+only_update = False
+files = os.listdir(output_dir)
+files.sort()
+only_update = len(files) > 0
+
+if only_update:
+    last_date = files[-1].split(".")[0]
+    last_write_daytime = date_to_day_time(last_date) + 86400
+    last_block = date_to_last_block(last_date)
+    print("only update from", last_block)
+    ERC20TransactionCsvs = prune_files(ERC20TransactionCsvs, last_block)
+
+    usdtupdate_daily_files = glob.glob("intermediate_data/usdtupdate_daily/*")
+    usdtupdate_daily_files.sort()
+    usdtupdate_daily_files.reverse()
+    for file in usdtupdate_daily_files:
+        lines = open(file).read().split("\n")[:-1]
+        print("read", file, "update", len(lines), len(usdt_of))
+        for line in lines:
+            arr = line.strip().split(",")
+            if arr[0] not in usdt_of:
+                usdt_of[arr[0]] = int(arr[1])
+
+print(datadir, ERC20TransactionCsvs)
 
 def add(to, value):
     if to not in usdt_of:
@@ -49,7 +71,10 @@ for file in ERC20TransactionCsvs:
     head = erc20TxCSV.readline()
 
 
-    erc20TxLine = erc20TxCSV.readline().strip()
+    if only_update:
+        erc20TxLine = read_first_line_gt_block(erc20TxCSV, last_block)
+    else:
+        erc20TxLine = erc20TxCSV.readline().strip()
 
     while (erc20TxLine!=""):
 
